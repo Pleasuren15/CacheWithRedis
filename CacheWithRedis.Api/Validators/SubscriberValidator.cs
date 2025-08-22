317f4593294d6051937f470fb968e1d9288e26a8
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-08-22 10:43:18 +0200

    src: basic validations added

---
+++ CacheWithRedis.Api/Validators/SubscriberValidator.cs
@@ -0,0 +1,25 @@
+﻿using CacheWithRedis.Api.Models;
+using FluentValidation;
+
+namespace CacheWithRedis.Api.Validators;
+
+public class SubscriberValidator : AbstractValidator<Subscriber>
+{
+    public SubscriberValidator()
+    {
+        RuleFor(s => s.FullName)
+            .Length(7, 20)
+            .NotEmpty()
+            .WithMessage("FullName value validation failed");
+
+        RuleFor(s => s.Email)
+            .NotEmpty()
+            .EmailAddress()
+            .WithMessage("Email value validation failed");
+
+        RuleFor(s => s.SubscriptionDate)
+            .NotNull()
+            .GreaterThan(DateTime.Now)
+            .WithMessage("SubscriptionDate value validation failed");
+    }
+}
